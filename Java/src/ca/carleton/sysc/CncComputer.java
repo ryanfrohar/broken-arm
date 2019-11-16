412f23f3e3114b1adf7b495cba2ed29a243e1758
hamakuk <kukai.beaudry@gmail.com>
2019-11-15 19:12:51 -0500

    Added log4j support. Fixed Arduino Transceiver.

--- Java/src/ca/carleton/sysc/CncComputer.java
+++ Java/src/ca/carleton/sysc/CncComputer.java
@@ -1,65 +1,32 @@
 package ca.carleton.sysc;
 
+import ca.carleton.sysc.common.types.Command;
+import ca.carleton.sysc.communication.ArduinoTransceiver;
 import ca.carleton.sysc.communication.MessageListener;
-import com.fazecast.jSerialComm.SerialPort;
-import com.fazecast.jSerialComm.SerialPortDataListener;
-import com.fazecast.jSerialComm.SerialPortEvent;
-import com.fazecast.jSerialComm.SerialPortMessageListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CncComputer {
 
+    private static final Logger LOG = LoggerFactory.getLogger(CncComputer.class);
+
     public static void main(final String[] args) {
 
-        // Check if Arduino is online
-        final SerialPort comPort = SerialPort.getCommPorts()[1];
-        comPort.openPort();
-
-        comPort.addDataListener(new SerialPortDataListener() {
-            @Override
-            public int getListeningEvents() { return SerialPort.LISTENING_EVENT_DATA_WRITTEN; }
-            @Override
-            public void serialEvent(SerialPortEvent event)
-            {
-                if (event.getEventType() == SerialPort.LISTENING_EVENT_DATA_WRITTEN)
-                    System.out.println("All bytes were successfully transmitted!");
-            }
-        });
-
-        comPort.addDataListener(new SerialPortMessageListener()
-        {
-            @Override
-            public int getListeningEvents() { return SerialPort.LISTENING_EVENT_DATA_RECEIVED; }
-
-            @Override
-            public byte[] getMessageDelimiter() { return new byte[] { (byte)0x0D }; }
-
-            @Override
-            public boolean delimiterIndicatesEndOfMessage() { return true; }
-
-            @Override
-            public void serialEvent(SerialPortEvent event)
-            {
-                byte[] delimitedMessage = event.getReceivedData();
-                System.out.println("Received the following delimited message: " + delimitedMessage);
-            }
-        });
-
-        // wake up grbl
-        byte[] bytes = "\r\n\r\n".getBytes();
-        comPort.writeBytes(bytes, bytes.length);
-        try {
-            Thread.sleep(2_000);
-        } catch (InterruptedException e) {
-            System.out.println("Thread interrupted, aborting read.");
-        }
-
-        comPort.closePort();
-
+        CncComputer.initArduino();
 
         // Begin the main thread that will listen to new inputs
         final MessageListener messageListener = new MessageListener();
-        System.out.println("Starting message listener...");
+        LOG.info("Starting message listener");
         messageListener.run();
+    }
 
+    /**
+     * Checks if the arduino is online
+     */
+    private static void initArduino() {
+        final ArduinoTransceiver arduinoIO = new ArduinoTransceiver();
+        arduinoIO.write(Command.WAKE_UP.getCode());
+        final String msg = arduinoIO.read();
+        LOG.info("Arduino GRBL online {}", msg);
     }
 }
--- Java/src/ca/carleton/sysc/common/types/Command.java
+++ Java/src/ca/carleton/sysc/common/types/Command.java
@@ -6,6 +6,7 @@ package ca.carleton.sysc.common.types;
 public enum Command {
 
     DEVELOPER("%s", CommandType.PARAMETERIZED),
+    WAKE_UP("\n\r\n\r", CommandType.NO_PARAMETER),
     STATUS("?", CommandType.NO_PARAMETER),
     PAUSE("!", CommandType.NO_PARAMETER),
     ABORT("M00", CommandType.NO_PARAMETER),
@@ -13,9 +14,15 @@ public enum Command {
     ESTOP("M112", CommandType.NO_PARAMETER),
     SEND_TEXT("", CommandType.PARAMETERIZED);
 
+    private static final String NEW_LINE = "\r\n";
+
     private final String code;
 
     private final CommandType commandType;
+
+    public byte[] getBytes() {
+        return (this.code + NEW_LINE).getBytes();
+    }
 
     Command(final String code, final CommandType commandType) {
         this.code = code;
