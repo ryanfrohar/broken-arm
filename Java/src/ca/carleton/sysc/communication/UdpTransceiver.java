bb680dce943ee0ee37eed60fa53162de3b56a822
tunjijnr1 <51492794+tunjijnr1@users.noreply.github.com>
2019-11-19 15:15:32 -0500

    programmed the udp receiver function

--- Java/src/ca/carleton/sysc/communication/UdpTransceiver.java
+++ Java/src/ca/carleton/sysc/communication/UdpTransceiver.java
@@ -1,40 +1,48 @@
 package ca.carleton.sysc.communication;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.Scanner;
 
 public class UdpTransceiver {
 // TODO Public receive and send
     private final static int PACKET_SIZE = 508; // Size of the datagram packet
     private final static int port = 1010;
 
+
+
     public DatagramPacket receive() {
         // Check the argument
-        try
-        {
+        try (DatagramSocket socket = new DatagramSocket(port)) {
             // Construct the socket
-            DatagramSocket socket = new DatagramSocket( port ) ;
 
-            for( ;; )
-            {
-                System.out.println( "Receiving on port " + port ) ;
-                DatagramPacket packet = new DatagramPacket( new byte[PACKET_SIZE], PACKET_SIZE ) ;
-                socket.receive( packet ) ;
+            System.out.println("Receiving on port " + port);
+            DatagramPacket packet = new DatagramPacket(new byte[PACKET_SIZE], PACKET_SIZE);
+            socket.receive(packet);
 
-                System.out.println( packet.getAddress() + " " + packet.getPort() + ": " + new String(packet.getData()).trim() ) ;
-                return packet;
-            }
+            System.out.println(packet.getAddress() + " " + packet.getPort() + ": " + new String(packet.getData()).trim());
+            return packet;
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        catch( Exception e )
-        {
-            System.out.println( e ) ;
-            return null;
-        }
+        System.out.println("There is an error i am returning null");
+      return null;
+
     }
 
 
     public void send( final DatagramPacket packet){
-
+        try (DatagramSocket socket = new DatagramSocket()) {
+            // Convert the arguments first, to ensure that they are valid
+            socket.send(packet);
+            System.out.println("Closing down");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 
 }
